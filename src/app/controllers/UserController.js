3f2ebc09590891b476aebc3e5b0f99ffc3e0b81d
makrau <iago006@hotmail.com>
2020-02-08 22:07:14 -0200

    implementado atualizacao do usuario

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -8,11 +8,51 @@ class UserController {
       });
 
       if (userExists) {
-        return res.status(400).json({ error: 'User already exists' });
+        return res.status(400).json({ error: 'Email already exists' });
       }
       const { id, name, email, provider } = await User.create(req.body);
 
       return res.json({ id, name, email, provider });
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const user = await User.findByPk(req.userId);
+
+      if (!user) {
+        return res.status(400).json({ error: 'User not found!' });
+      }
+
+      const { email, oldPassword } = req.body;
+
+      if (email && email !== user.email) {
+        const emailExists = await User.findOne({ where: { email } });
+
+        if (emailExists) {
+          return res.status(400).json({ error: 'Email already exists' });
+        }
+      }
+
+      if (!oldPassword && req.body.password) {
+        return res.status(401).json({
+          error: 'Cannot change password without passing the previous password',
+        });
+      }
+
+      if (oldPassword && !(await user.checkPassword(oldPassword))) {
+        return res.status(401).json({ error: 'Password does not match' });
+      }
+
+      const { id, name, provider } = await user.update(req.body);
+      return res.json({
+        id,
+        name,
+        email,
+        provider,
+      });
     } catch (error) {
       return res.status(400).json({ error: error.message });
     }
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import auth from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.put('/users', auth, UserController.update);
 
 routes.post('/sessions', SessionController.store);
 
