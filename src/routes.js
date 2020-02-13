e90d06560d43d5d1db40d8dd1e3a6e4bb0948442
makrau <iago006@hotmail.com>
2020-02-12 23:05:35 -0200

    criada index de providers

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+
 import auth from './app/middlewares/auth';
 
 const routes = new Router();
@@ -15,5 +17,7 @@ routes.put('/users', auth, UserController.update);
 routes.post('/sessions', SessionController.store);
 
 routes.post('/files', auth, upload.single('file'), FileController.store);
+
+routes.get('/providers', auth, ProviderController.index);
 
 export default routes;
