141b074a67d7b225cebf28beef83349976104427
makrau <iago006@hotmail.com>
2020-02-13 23:11:14 -0200

    Criando notificacao

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,10 @@
-import { startOfHour, parseISO } from 'date-fns';
+import { startOfHour, parseISO, format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { createAppointmentValidation } from '../validations/appointment';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -52,11 +54,26 @@ class AppointmentController {
         return res.status(400).json({ error: 'provider_id is not a provider' });
       }
 
+      const hourStart = startOfHour(parseISO(date));
+
       const appointment = await Appointment.create({
         user_id: req.userId,
         provider_id,
-        date: startOfHour(parseISO(date)),
+        date: hourStart,
       });
+
+      const user = await User.findByPk(req.userId);
+      const formattedDate = format(
+        hourStart,
+        "'dia' dd 'de' MMMM', às' H:mm'h'",
+        { locale: pt }
+      );
+
+      await Notification.create({
+        content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
+        user: provider_id,
+      });
+
       return res.json(appointment);
     } catch (error) {
       return res.status(400).json({ error: error.message });
